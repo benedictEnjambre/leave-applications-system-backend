8491ddff659743d792f9423a0c35071203d7f1be
bene-byte <benedict.enjambre@synacy.com>
2025-09-01 17:54:39 +0800

    Initial commit - Leaves Management backend

---
+++ src/main/java/com/synacy/leavesmanagement/LeavesmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.synacy.leavesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeavesmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeavesmanagementApplication.class, args);
+	}
+
+}
