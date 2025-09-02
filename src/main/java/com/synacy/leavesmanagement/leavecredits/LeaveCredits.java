0c8cb1cbada92f891e8f805d3d71d4f9efcbfeae
mariedasian22 <marie.dasian@synacy.com>
2025-09-02 10:09:31 +0800

    feat: create LeaveCredits entity with user mapping and credit fields

--- src/main/java/com/synacy/leavesmanagement/leavecredits/LeaveCredits.java
+++ src/main/java/com/synacy/leavesmanagement/leavecredits/LeaveCredits.java
@@ -1,31 +1,29 @@
 package com.synacy.leavesmanagement.leavecredits;
 
-import com.synacy.leavesmanagement.user.User;
 import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.synacy.leavesmanagement.user.User;
 
 @Entity
 @Setter
 @Getter
 @NoArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class LeaveCredits {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "leave_credits_sequence")
     @SequenceGenerator(name = "leave_credits_sequence", sequenceName = "leave_credits_sequence", allocationSize = 1)
-    private Long id;
+    private Long creditId;
 
     @OneToOne
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 
     @Column(nullable = false)
-    private int totalCredits = 30;
+    private int totalCredits;
 
     @Column(nullable = false)
-    private int remainingCredits = 30;
+    private int remainingCredits;
 }
