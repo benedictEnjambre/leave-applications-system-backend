f3a56ce9e3a3da3d4abc58ad138737f963305d5b
bene-byte <benedict.enjambre@synacy.com>
2025-09-03 12:38:32 +0800

    Test: Spock testing Config and Code Trial

--- src/main/java/com/synacy/leavesmanagement/user/UserRequest.java
+++ src/main/java/com/synacy/leavesmanagement/user/UserRequest.java
@@ -1,9 +1,11 @@
 package com.synacy.leavesmanagement.user;
 
 import com.synacy.leavesmanagement.leavecredits.LeaveCredits;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UserRequest {
     private String name;
     private Role role;
