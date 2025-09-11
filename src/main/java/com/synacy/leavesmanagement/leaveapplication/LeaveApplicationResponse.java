09e37cb8e5f101139ca12ff3598e43e9a5c42747
Nick-Synacy <vincent.malazarte@synacy.com>
2025-09-11 15:44:48 +0800

    - minor addition

--- src/main/java/com/synacy/leavesmanagement/leaveapplication/LeaveApplicationResponse.java
+++ src/main/java/com/synacy/leavesmanagement/leaveapplication/LeaveApplicationResponse.java
@@ -8,6 +8,7 @@ public class LeaveApplicationResponse {
 
     private final Long id;
     private final String employeeName;
+    private final String approverName;
     private final LocalDate startDate;
     private final LocalDate endDate;
     private final LeaveStatus status;
@@ -19,6 +20,7 @@ public class LeaveApplicationResponse {
     public LeaveApplicationResponse(LeaveApplication leaveApplication) {
         this.id = leaveApplication.getId();
         this.employeeName = leaveApplication.getEmployee().getName();
+        this.approverName = leaveApplication.getApprover().getName();
         this.startDate = leaveApplication.getStartDate();
         this.endDate = leaveApplication.getEndDate();
         this.status = leaveApplication.getStatus();
